575b831b0de0d982fe46324ca94458a3edb997f7
Tabish Ali <149582990+iksfsf@users.noreply.github.com>
2026-09-23 16:41:42 +0500

    Add files via upload
    Complete Day 1 Python basics

---
+++ day01_final.py
@@ -0,0 +1,23 @@
+name="Tabish"
+age=22
+city="New York"
+university="Stanford University"
+degree="Computer Science"
+current_semester=3
+gpa=3.8
+daily_study_hours=2
+is_student=True
+has_completed_python=True
+
+
+print(type(name), type(age), type(city), type(university), type(degree), type(current_semester), type(gpa), type(daily_study_hours), type(is_student), type(has_completed_python))
+
+weekly_study_hours=daily_study_hours * 7
+print("Weekly study hours:", weekly_study_hours)
+
+
+x = 22
+y = "22"
+
+print(type(x))
+print(type(y))
---
+++ day01_variables.py
@@ -0,0 +1,10 @@
+
+
+
+name="Tabish"
+age=22
+university= "ITU"
+learning_goal="AI Engineer"
+daily_hours=3
+
+print(name,age,university,learning_goal,daily_hours)
