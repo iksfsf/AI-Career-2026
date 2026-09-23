6f51e40918302389793d8eddb5c0f99d98c5c3c9
Tabish Ali <149582990+iksfsf@users.noreply.github.com>
2026-09-23 22:12:10 +0500

    Add files via upload
    Complete Day 2 conditions loops and grade calculator

---
+++ day02_conditions.py
@@ -0,0 +1,43 @@
+
+age=22
+if(age>=18):
+    print("adult")
+    
+else:
+    print("minor")
+    
+    
+    
+marks = 75
+    
+if(marks>=80):
+    print("A")
+elif(marks>=70 and marks<=79):
+    print("B")
+elif(marks>=60 and marks<=69):
+    print("C")
+elif(marks>=50 and marks<=59):
+    print("D")
+else:
+    print("F")
+    
+    
+cgpa=3.00
+if(cgpa>=3):
+    print("Good")
+else:
+    print("need improvement")
+    
+    
+for i in range(1,11):
+        print(i)
+        
+        
+    
+for i in range(1,11):
+    print(i*i)
+    
+    
+    
+for i in range(5):
+    print("Tabish")
---
+++ day02_grade_calculator.py
@@ -0,0 +1,40 @@
+
+
+marks=75
+
+if(marks>=80):
+    print("A")
+elif(marks>=70 and marks<=79):
+    print("B")
+elif(marks>=60 and marks<=69):
+    print("C")
+elif(marks>=50 and marks<=59):
+    print("D")
+else:
+    print("F")
+    
+    
+Python_marks = 90
+Math_marks = 80
+English_marks = 70
+Programming_marks = 60
+Database_marks = 50
+
+
+total_marks = Python_marks + Math_marks + English_marks + Programming_marks + Database_marks    
+print(total_marks)
+
+average=total_marks/5
+print(average)
+
+if(average>=80):
+    print("A")
+elif(average>=70):
+    print("B")
+elif(average>=60) :
+    print("C")
+elif(average>=50):
+    print("D")
+else:
+    print("F")
+    
